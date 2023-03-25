e21487775bed79012f6fa749d8b0590cb7f4eaf4
varun <varunchander96@gmail.com>
2023-03-25 12:50:56 +0530

    git distinct charactesr.

--- SampleCases/src/A.java
+++ SampleCases/src/A.java
@@ -23,7 +23,7 @@ public class A {
 		}
 		
 		long cnt = hSet.stream().count();
-		hSet.stream().forEach(System.out::println);
+		hSet.stream().distinct().forEach(System.out::println);
 		
 		System.out.print("character count in given string is : "+cnt);
 		
